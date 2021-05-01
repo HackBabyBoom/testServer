0fe03ca182d258e4d6c1bf6bfd0550bda69385fd
diydriller <dhrhd080@naver.com>
2021-05-02 03:48:59 +0900

    commit

--- index.js
+++ index.js
@@ -31,6 +31,60 @@ app.get('/main',(req,res)=>{
     })
 })
 
+app.get('/transaction',(req,res)=>{
+    return res.json({
+        month_consumption:"1,204,800",
+        month_gain:"3,042,000",
+        bank_consumptions:[
+            {
+                percent:24,
+                card:{
+                    card_name:"하나체크카드",
+                    card_consumption:"293,800"
+                }
+            },
+            {
+                percent:25,
+                card:{
+                    card_name:"신한체크카드",
+                    card_consumption:"304,400"
+                }
+            },
+            {
+                percent:23,
+                card:{
+                    card_name:"IBK기업체크카드",
+                    card_consumption:"287,500"
+                }
+            },
+            {
+                percent:26,
+                card:{
+                    card_name:"우리체크카드",
+                    card_consumption:"319,500"
+                }
+            }
+        ],
+        stores:[
+            {
+                img_url:"https://babyboom.s3-ap-northeast-1.amazonaws.com/Group+3214.png",
+                name:"스타벅스",
+                consumption:"42,5000"
+            },
+            {
+                img_url:"https://babyboom.s3-ap-northeast-1.amazonaws.com/Group+3217.png",
+                name:"교촌치킨",
+                consumption:"42,5000"
+            },
+            {
+                img_url:"https://babyboom.s3-ap-northeast-1.amazonaws.com/Group+3218.png",
+                name:"교보문고",
+                consumption:"42,0000"
+            }
+        ]
+    })
+})
+
  app.listen(process.env.PORT||3000,()=>{
     console.log('server is listening'); 
  });
