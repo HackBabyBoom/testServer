c126bb42e7474ecf9a981bb2d8e97c820586c27b
diydriller <dhrhd080@naver.com>
2021-05-02 01:45:55 +0900

    commit

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.get('/',(req,res)=>{
     return res.send("hello developers");
 });
 
+app.get('/main',(req,res)=>{
+    return res.json({
+        accounts:[
+            {
+                number:"1002-255-134576",
+                name:"주거래 s20통장",
+                money:"72,740,000원"
+            },
+            {
+                number:"1002-255-134574",
+                name:"주거래 s22통장",
+                money:"34,340,000원"
+            }
+        ],
+        month_total_transaction:"860,000원",
+        month_due:"260,000원"
+    })
+})
+
  app.listen(process.env.PORT||3000,()=>{
     console.log('server is listening'); 
  });
